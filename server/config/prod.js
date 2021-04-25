0893bc6bb48c2358c3842d5954975d4103d5e366
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-04-25 20:22:16 +0900

    Style : Video Upload Page UI

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,8 @@
 module.exports = {
-    mongoURI:process.env.MONGO_URI
-}
+  mongoURI: process.env.MONGO_URI,
+};
+
+// module.exports = {  dev.js 생성후 복붙
+//   mongoURI:
+//     "mongodb+srv://ksmfou98:비밀번호@video-platform.p1qzb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+// };
