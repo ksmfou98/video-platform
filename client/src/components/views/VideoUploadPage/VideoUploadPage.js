c5dc64accf683b668be5f32f2919e06cb27e63fc
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-04-27 21:55:07 +0900

    Implements : file upload Api

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Typography, Button, Form, message, Input, Icon } from "antd";
 import Dropzone from "react-dropzone";
+import Axios from "axios";
 
 const { TextArea } = Input;
 const { Title } = Typography;
@@ -43,6 +44,25 @@ const VideoUploadPage = () => {
     setCategory(e.target.value);
   };
 
+  const onDrop = (files) => {
+    // 이 과정은 백앤드에 파일을 보내기 위한 것
+
+    let formData = new FormData();
+    const config = {
+      header: { "content-type": "multipart/form-data" },
+    };
+    formData.append("file", files[0]);
+
+    Axios.post("/api/video/uploadfiles", formData, config)
+      .then(response => {
+        if (response.data.success){
+          console.log(response.data)
+        } else{
+          alert("비디오 업로드를 실패했습니다")
+        }
+      })
+  };
+
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
@@ -53,7 +73,7 @@ const VideoUploadPage = () => {
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           {/* Drop zone */}
 
-          <Dropzone onDrop multiple maxSize>
+          <Dropzone onDrop={onDrop} multiple={false} maxSize={100000000}>
             {({ getRootProps, getInputProps }) => (
               <div
                 style={{
