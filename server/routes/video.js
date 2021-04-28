22d7a2660c97ab46003a5858da99b82f3295b33a
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-04-28 13:08:02 +0900

    Implements : video 썸네일

--- server/routes/video.js
+++ server/routes/video.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const ffmpeg = require("fluent-ffmpeg");
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -32,6 +33,37 @@ router.post("/uploadfiles", (req, res) => {
       fileName: res.req.file.filename,
     });
   });
+});
+
+router.post("/thumbnail", (req, res) => {
+  // 썸네일 생성하고 비디오 러닝타임도 가져오기
+
+  ffmpeg(req.body.url)
+    .on("filenames", function (filenames) {
+      console.log("Will generate " + filenames.join(", "));
+      console.log(filenames);
+
+      filePath = "uploads/thumbnails/" + filenames[0];
+    })
+    .on("end", function () {
+      console.log("Screenshots taken");
+      return res.json({
+        success: true,
+        url: filePath,
+        fileName: filenames,
+        fileDuration: fileDuration,
+      });
+    })
+    .on("error", function (err) {
+      console.error(err);
+      return res.json({ success: false, err });
+    })
+    .screenshots({
+      count: 3,
+      folder: "uploads/thumbnails",
+      size: "320x240",
+      filename: "thumbnail-%b.png",
+    });
 });
 
 module.exports = router;
