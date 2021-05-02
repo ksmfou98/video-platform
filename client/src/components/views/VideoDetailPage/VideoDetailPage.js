1611f754f13c03a79be3b1d0b3699166fb032200
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-05-02 18:17:52 +0900

    Styles : 구독 버튼

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Row, Col, List, Avatar } from "antd";
 import Axios from "axios";
 import SideVideo from "./Sections/SideVideo";
+import Subscribe from "./Sections/Subscribe";
+
 
 const VideoDetailPage = ({ match }) => {
   const videoId = match.params.videoId;
@@ -31,7 +33,7 @@ const VideoDetailPage = ({ match }) => {
               controls
             />
 
-            <List.Item actions>
+            <List.Item actions = {[<Subscribe userTo={VideoDetail.writer._id} />]}>
               <List.Item.Meta
                 avatar={<Avatar src={VideoDetail.writer.image} />}
                 title={VideoDetail.writer.name}
