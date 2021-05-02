3512288cff2745838560eb10f977c77645564c5e
이도현 <64634992+ksmfou98@users.noreply.github.com>
2021-05-02 14:09:57 +0900

    Implements : SideVideo

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Row, Col, List, Avatar } from "antd";
 import Axios from "axios";
+import SideVideo from "./Sections/SideVideo";
 
 const VideoDetailPage = ({ match }) => {
   const videoId = match.params.videoId;
@@ -42,7 +43,7 @@ const VideoDetailPage = ({ match }) => {
           </div>
         </Col>
         <Col lg={6} xs={24}>
-          Side Video
+          <SideVideo />
         </Col>
       </Row>
     );
